78e20ed1cec5a12e2ec33a6ad9d9ee6ca28adc7a
ieunune <une@kakao.com>
2023-05-18 23:06:25 +0900

    Quiz App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import QuizView from './QuizView';
+import ResultView from './ResultView';
+
+import quiz from './quiz';
+
 import './App.css';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [sheet, setSheet] = useState([]);
+
+  const currentQuiz = quiz[currentPage];
+  const currentAnswer = sheet[currentPage];
+
+  function handleClickPrev() {
+    setCurrentPage(currentPage - 1);
+  }
+
+  function handleClickNext() {
+    setCurrentPage(currentPage + 1);
+  }
+
+  function handleClickChoice(item) {
+    const updatedSheet = [...sheet];
+
+    updatedSheet[currentPage] = item;
+    setSheet(updatedSheet);
+  }
+
+  function handleClickHome() {
+    setCurrentPage(0);
+    setSheet([]);
+  }
+
+  const percent = (currentPage / quiz.length) * 100;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div
+        className='App-progressBar'
+        style={{
+          width: `${percent}%`,
+        }}
+      />
+      {currentPage !== quiz.length ? (
+        <QuizView
+          currentQuiz={currentQuiz}
+          currentAnswer={currentAnswer}
+          handleClickChoice={handleClickChoice}
+          handleClickPrev={handleClickPrev}
+          handleClickNext={handleClickNext}
+          currentPage={currentPage}
+        />
+      ) : (
+        <ResultView
+          quiz={quiz}
+          sheet={sheet}
+          handleClickHome={handleClickHome}
+        />
+      )}
     </div>
   );
 }
